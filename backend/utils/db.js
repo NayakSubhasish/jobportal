fbd9a63cdc950b724a998b502083a35ab96f1fc0
SUBHASISH NAYAK <153982823+NayakSubhasish@users.noreply.github.com>
2024-08-04 17:39:45 +0530

    Update db.js

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -8,7 +8,6 @@ const connectDB = async () => {
             serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
             socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
             bufferCommands: false, // Disable mongoose buffering
-            bufferMaxEntries: 0 // and MongoDB driver buffering
         });
         console.log('mongodb connected successfully');
     } catch (error) {
